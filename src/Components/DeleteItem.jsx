6a9984f35079a842dbe17c5c055214a50274cbef
joephillpot <joephillpot@gmail.com>
2024-11-08 17:42:54 +0000

    Added functionality to view topics and see articles related to topics

--- src/Components/DeleteItem.jsx
+++ src/Components/DeleteItem.jsx
@@ -1,33 +1,33 @@
-import { useState } from "react";
-import { deleteItem } from "../utils/api";
-import { Loading } from "./Loading";
+import { useState } from 'react';
+import { deleteItem } from '../utils/api';
+import { Loading } from './Loading';
 
-export const DeleteItem = ({id, itemToDelete, setCommentChanged}) => {
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState(false)
+export const DeleteItem = ({ id, itemToDelete, setCommentChanged }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const handleDelete = () => {
-    setIsLoading(true)
-    deleteItem(id, itemToDelete).then(() => {
-      setIsLoading(false)
-      setError(null)
-      setCommentChanged(true)
-    }).catch((err)=> {
-      setError(err)
-    })
-  }
+    setIsLoading(true);
+    deleteItem(id, itemToDelete)
+      .then(() => {
+        setIsLoading(false);
+        setError(null);
+        setCommentChanged(true);
+      })
+      .catch((err) => {
+        setError(err);
+        setIsLoading(false);
+      });
+  };
 
-  if(isLoading) {
-    return (
-      <Loading />
-    )
+  if (isLoading) {
+    return <Loading />;
   }
-
 
   return (
     <div className="delete-button">
       <button onClick={handleDelete}>Delete</button>
       {error ? <p>Error try again</p> : null}
     </div>
-  )
-}
+  );
+};
--- src/Components/articles/solo-article/Article.jsx
+++ src/Components/articles/solo-article/Article.jsx
@@ -1,4 +1,5 @@
-import { Votes } from "../../votes/Votes";
+import { Votes } from '../../votes/Votes';
+import { Link } from 'react-router-dom';
 
 export const Article = (props) => {
   const { article } = props;
@@ -6,12 +7,12 @@ export const Article = (props) => {
   return (
     <article>
       <h4>posted by {article.author}</h4>
-      <h3>{article.topic}</h3>
+      <Link to={`/topics/${article.topic}`}><h3>{article.topic}</h3></Link>
       <h3>posted on {article.created_at}</h3>
       <h2>{article.title}</h2>
       <p>{article.body}</p>
       <img src={article.article_img_url} alt={`${article.title} image`} />
-      <Votes votes={article.votes} id={article.article_id} itemToUpdate='articles' />
+      <Votes votes={article.votes} id={article.article_id} itemToUpdate="articles" />
     </article>
   );
 };
